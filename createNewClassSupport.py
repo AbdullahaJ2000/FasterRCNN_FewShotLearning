c4bc286eda7dac9762720838427f7a7f955e87f0
unknown <abdullah.101151@gmail.com>
2025-05-18 15:18:17 +0300

    fix create init data new class

--- createNewClassSupport.py
+++ createNewClassSupport.py
@@ -3,16 +3,15 @@ import json
 with open('C:/Users/User/Desktop/few_shot_detection/kaggle/input/oxalis-coco/_annotations.coco.json', 'r') as f:
     data = json.load(f)
 
-# نأخذ أول 10 صور فط
+
 first_10_images = data['images'][:10]
 
-# نحصل على الـids الخاصة بهذه الصور
+
 first_10_image_ids = set(img['id'] for img in first_10_images)
 
-# نفلتر التعليقات التي تخص هذه الصور فقط
+
 filtered_annotations = [ann for ann in data['annotations'] if ann['image_id'] in first_10_image_ids]
 
-# نعيد بناء البيانات مع الصور والتعليقات المفلترة
 new_data = {
     "info": data.get("info", {}),
     "licenses": data.get("licenses", []),
@@ -21,8 +20,6 @@ new_data = {
     "annotations": filtered_annotations
 }
 
-# حفظ الملف الجديد
+
 with open('C:/Users/User/Desktop/few_shot_detection/kaggle/input/oxalis-coco/_annotations_first_10.coco.json', 'w') as f:
     json.dump(new_data, f, indent=4)
-
-print("تم إنشاء ملف JSON يحتوي أول 10 صور فقط: _annotations_first_10.coco.json")
